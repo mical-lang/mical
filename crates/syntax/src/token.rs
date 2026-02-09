95a93b96a430cd1de6a6faa4e72e55ab2a4aee01
ryota2357 <noreply@ryota2357.com>
2026-02-09 17:03:52 +0900

    feat(syntax): define tokens

---
+++ crates/syntax/src/token.rs
@@ -0,0 +1,43 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub len: u32,
+}
+
+const _: () = {
+    assert!(size_of::<Token>() == size_of::<u64>());
+    assert!(size_of::<Token>() == size_of::<Option<Token>>());
+};
+
+pub trait TokenStream<'src>: Iterator<Item = Token> {
+    fn source(&self) -> &'src str;
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+pub enum TokenKind {
+    Word,
+    Integer { base: NumBase, is_empty: bool },
+    True,
+    False,
+
+    Tab,     // 0x09
+    Newline, // 0x0A
+    Space,   // 0x20
+
+    Backslash,  // \
+    CloseBrace, // }
+    Greater,    // >
+    Minus,      // -
+    OpenBrace,  // {
+    Pipe,       // |
+    Plus,       // +
+    Sharp,      // #
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum NumBase {
+    Binary = 2,
+    Octal = 8,
+    Decimal = 10,
+    Hexadecimal = 16,
+}

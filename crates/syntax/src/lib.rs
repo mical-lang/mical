55d82239a6ca9abd8d54abc40610d09a1ec6557b
ryota2357 <noreply@ryota2357.com>
2026-02-10 12:11:00 +0900

    feat(syntax): add ast and syntax kind definitions

--- crates/syntax/src/lib.rs
+++ crates/syntax/src/lib.rs
@@ -1,1 +1,28 @@
+pub mod syntax_kind;
+pub use syntax_kind::*;
+
+pub mod ast;
 pub mod token;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MicalLanguage {}
+
+impl rowan::Language for MicalLanguage {
+    type Kind = SyntaxKind;
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        SyntaxKind::from(raw)
+    }
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        kind.into()
+    }
+}
+
+pub type SyntaxNode = rowan::SyntaxNode<MicalLanguage>;
+pub type SyntaxToken = rowan::SyntaxToken<MicalLanguage>;
+pub type SyntaxElement = rowan::SyntaxElement<MicalLanguage>;
+pub type SyntaxNodeChildren = rowan::SyntaxNodeChildren<MicalLanguage>;
+pub type SyntaxElementChildren = rowan::SyntaxElementChildren<MicalLanguage>;
+pub type Preorder = rowan::api::Preorder<MicalLanguage>;
+pub type PreorderWithTokens = rowan::api::PreorderWithTokens<MicalLanguage>;
+pub type SyntaxNodePtr = rowan::ast::SyntaxNodePtr<MicalLanguage>;
+pub use rowan::{GreenNode, TextLen, TextRange, TextSize};

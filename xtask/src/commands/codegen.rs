3836092e89c1c18c51009cec505114d8681ee548
ryota2357 <noreply@ryota2357.com>
2026-02-10 12:07:40 +0900

    feat(xtask): add codegen task

---
+++ xtask/src/commands/codegen.rs
@@ -0,0 +1,52 @@
+mod syntax;
+
+use crate::{Level, flags, message, project_root};
+use anyhow::{Result, anyhow};
+use std::{fs, path::Path};
+use xshell::{Shell, cmd};
+
+impl flags::Codegen {
+    pub(crate) fn run(self, sh: &Shell) -> Result<()> {
+        sh.change_dir(project_root());
+        let kind = self.kind.unwrap_or_default();
+        let check = self.check;
+        match kind {
+            flags::CodegenKind::All => {
+                syntax::generate(sh, check)?;
+                Ok(())
+            }
+            flags::CodegenKind::Syntax => syntax::generate(sh, check),
+        }
+    }
+}
+
+fn rustfmt(sh: &Shell, text: String) -> Result<String> {
+    let rustfmt_toml = project_root().join("rustfmt.toml");
+    let stdout = cmd!(sh, "rustfmt --config-path {rustfmt_toml} --config max_width=120")
+        .stdin(text)
+        .read()?;
+    if !stdout.ends_with('\n') { Ok(format!("{stdout}\n")) } else { Ok(stdout) }
+}
+
+fn display_path(path: &Path) -> std::path::Display<'_> {
+    path.strip_prefix(project_root()).unwrap_or(path).display()
+}
+
+fn ensure_file_contents(path: &Path, contents: &str, check: bool) -> Result<()> {
+    if let Ok(old_contents) = fs::read_to_string(path) {
+        if old_contents == contents {
+            message(Level::Info, format!("{} is up to date", display_path(path)));
+            return Ok(());
+        } else if check {
+            message(Level::Error, format!("{} is out of date", display_path(path)));
+            return Err(anyhow!("failed to check generated file"));
+        }
+    }
+
+    if let Some(parent) = path.parent() {
+        fs::create_dir_all(parent)?;
+    }
+    message(Level::Warn, format!("updating {}", display_path(path)));
+    fs::write(path, contents)?;
+    Ok(())
+}
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,12 +1,20 @@
 mod flags;
 
+mod utils;
+use utils::*;
+
+mod commands;
+
 use anyhow::Result;
+use xshell::Shell;
 
 fn main() -> Result<()> {
     let flags = {
         let cli_args = std::env::args_os().skip(1).collect::<Vec<_>>();
         flags::Xtask::from_vec(cli_args)?
     };
-    println!("{:?}", flags);
-    Ok(())
+    let sh = Shell::new()?;
+    match flags.subcommand {
+        flags::XtaskCmd::Codegen(cmd) => cmd.run(&sh),
+    }
 }

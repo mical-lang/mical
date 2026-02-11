4e81122dfc6c96790a39ed3746db063972d2c57d
ryota2357 <noreply@ryota2357.com>
2026-02-11 10:29:24 +0900

    feat(syntax): add SyntaxError

---
+++ crates/syntax/src/syntax_error.rs
@@ -0,0 +1,28 @@
+use rowan::TextRange;
+use std::{borrow::Cow, fmt};
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct SyntaxError {
+    message: Cow<'static, str>,
+    range: TextRange,
+}
+
+impl SyntaxError {
+    pub fn new(message: impl Into<Cow<'static, str>>, range: TextRange) -> Self {
+        Self { message: message.into(), range }
+    }
+
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+
+    pub fn range(&self) -> TextRange {
+        self.range
+    }
+}
+
+impl fmt::Display for SyntaxError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "{:?}: {}", self.range, &self.message)
+    }
+}
